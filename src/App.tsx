df5ef173838d1aba4928d6dd84f1d374a6663870
Julieth Guzmán <juli.caroguzman06@gmail.com>
2024-12-05 22:27:59 -0500

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p className="flex text-cyan-200">
+      CV
+    </p>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //el punto es la carpeta principal, los / es para diferencia la siguiente carpeta, los * me indican que recorren las carpetas para poder buscar el archivo requerido
+  content: ["./src/**/*.{html,tsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
