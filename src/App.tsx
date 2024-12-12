8ecfeb83856a358b94b716c0b3fdc82d444a0833
Julieth Guzmán <juli.caroguzman06@gmail.com>
2024-12-11 22:20:15 -0500

    avance cv

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <p className="flex text-cyan-200">
-      CV
-    </p>
+    <div className="flex flex-col w-full px-20">
+      <header className="flex flex-col flex-1 w-full items-center">
+        <div className="relative flex w-full h-[100px] flex-row items-center justify-center">
+          <p className="flex w-[200px]">perfil</p>
+          <p className="absolute right-0 top-0">titulo</p> 
+        </div>
+        <div>info</div>
+      </header>
+      <section className="flex flex-grow flex-1">
+        otro
+      </section>
+    </div>
   )
 }
 
